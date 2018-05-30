a1a7b16a136207f72047bbcae3e8c006573ba501
dongjiawei316 <dongjiawei316@126.com>
2018-05-30 21:15:37 +0800

    增加了坐标轴的描述

--- 3d_draws.py
+++ 3d_draws.py
@@ -3,31 +3,27 @@ from matplotlib import pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-fig = plt.figure()
+
+fig = plt.figure(figsize=(8,8), dpi=400)
 ax = Axes3D(fig)
 len = 8;
 step = 0.4;
 
 def build_staticslayer(z_value):
-    x = np.arange(0, 0.5, 0.01);
+    x = np.arange(0, 50, 1);
     y = np.arange(1, 250, 5);
 
     y1 = np.divide(1000, y)
     y2 = np.rint(y1)
 
+    x1 = x / 100
+
+
     x, y = np.meshgrid(x, y)
-    z = (x *( (2*x - x * x)**(1000/y)))
+    z = (x1 *( (2*x1 - x1 * x1)**(1000/y)))*100
 
-  #  x1 = np.multiply(2, x)
-  #  x2 = np.power(x, 2)
-  #  x3 = np.subtract(x1, x2)
-# x4 = np.power(x3, y1)
- #   z1 = np.multiply(x4, x)
-  #  z2, z3 = np.meshgrid(z1, x)
-  #  z = z2 + z3;
-
-    print(y2)
-    print(z)
+    #print(y2)
+    #print(z)
     return (x, y, z);
 
 def build_layer(z_value):
@@ -52,22 +48,13 @@ def build_gaussian_layer(mean, standard_deviation):
 # 具体函数方法可用 help(function) 查看，如：help(ax.plot_surface)
 x1, y1, z1 = build_staticslayer(0.2);
 ax.plot_surface(x1, y1, z1, rstride=1, cstride=1, color='green')
-#ax.plot_wireframe(x1, y1, z1, rstride=1, cstride=1, color='green')
 
-#x5, y5, z5 = build_layer(0.15);
-#ax.plot_surface(x5, y5, z5, rstride=1, cstride=1, color='pink')
-
-# x2, y2, z2 = build_layer(-0.26);
-# ax.plot_surface(x2, y2, z2, rstride=1, cstride=1, color='yellow')
-#
-# x6, y6, z6 = build_layer(-0.22);
-# ax.plot_surface(x6, y6, z6, rstride=1, cstride=1, color='pink')
-
-# x4, y4, z4 = build_layer(0);
-# ax.plot_surface(x4, y4, z4, rstride=1, cstride=1, color='purple')
-
-#x3, y3, z3 = build_gaussian_layer(0, 1)
-#ax.plot_surface(x3, y3, z3, rstride=1, cstride=1, cmap='rainbow')
-#ax.view_init(elev=30,azim=10)#改变绘制图像的视角,即相机的位置,azim沿着z轴旋转，elev沿着y轴
 ax.autoscale_view()
-plt.show()
+my_z_ticks = np.arange(0, 14, 0.5)
+ax.set_zticks(my_z_ticks)
+ax.set_xlabel("original loss rate (%) ")
+ax.set_ylabel('looptime (ms)')
+ax.set_zlabel("final loss rate (%s)")
+
+#plt.show()
+plt.savefig('loss_rate_3D.png', bbox_inches = 'tight')
